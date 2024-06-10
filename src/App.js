8dec59d80f17ab74854d1f29237c72bc699fc701
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-06-10 19:23:39 +0200

    Imported bootstrap to project

--- src/App.js
+++ src/App.js
@@ -3,29 +3,36 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App 2.0</h1>
-      <footer>
-        Coded by{" "}
-        <a href="https://github.com/RRachelvd" target="_blank" rel="noreferrer">
-          Rachèl van Dijkhorst
-        </a>
-        , open-sourced on{" "}
-        <a
-          href="https://github.com/RRachelvd/weather-app-2.0"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://react-weather-app-by-rachel.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App 2.0</h1>
+        <div className="btn btn-primary">hello</div>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://github.com/RRachelvd"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rachèl van Dijkhorst
+          </a>
+          , open-sourced on{" "}
+          <a
+            href="https://github.com/RRachelvd/weather-app-2.0"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://react-weather-app-by-rachel.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
