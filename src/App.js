20fc11c00509ce36876280e2b6ae8b8b1b55eb09
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-06-10 17:51:30 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App 2.0</div>;
+  return (
+    <div className="App">
+      <div>Weather App 2.0</div>
+      <footer>
+        Coded by
+        <a href="https://github.com/RRachelvd" target="_blank" rel="noreferrer">
+          Rachèl van Dijkhorst
+        </a>
+        , open-sourced on
+        <a
+          href="https://github.com/RRachelvd/weather-app-2.0"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+        and hosted on
+        <a
+          href="https://react-weather-app-by-rachel.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
