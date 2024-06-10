1fe1c139ff125e582da80f25f168b81779e70402
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-06-10 17:57:53 +0200

    Added spacing with right syntax

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ export default function App() {
     <div className="App">
       <h1>Weather App 2.0</h1>
       <footer>
-        Coded by
+        Coded by {""}
         <a href="https://github.com/RRachelvd" target="_blank" rel="noreferrer">
           Rachèl van Dijkhorst
         </a>
-        ,{``} open-sourced on
-        {``}
+        , open-sourced on
+        {""}
         <a
           href="https://github.com/RRachelvd/weather-app-2.0"
           target="_blank"
@@ -18,9 +18,9 @@ export default function App() {
         >
           Github
         </a>
-        {``}
+        {""}
         and hosted on
-        {``}
+        {""}
         <a
           href="https://react-weather-app-by-rachel.netlify.app/"
           target="_blank"
