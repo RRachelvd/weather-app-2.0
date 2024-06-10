f8faa74ee803fa3509565b94cfcad144626da1ea
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-06-10 21:25:05 +0200

    Updated links in footer, removed bootstrap css for now

--- src/App.js
+++ src/App.js
@@ -5,46 +5,48 @@ import WeatherApp from "./WeatherApp";
 
 export default function App() {
   return (
-    <div className="weather-app-container">
-      <header>
-        <Form />
-      </header>
-      <main>
-        <WeatherApp />
-        <Forecast />
-        <div className="weather-forecast" id="forecast"></div>
-      </main>
+    <div className="container">
+      <div className="weather-app-container">
+        <header>
+          <Form />
+        </header>
+        <main>
+          <WeatherApp />
+          <Forecast />
+          <div className="weather-forecast" id="forecast"></div>
+        </main>
 
-      <footer>
-        <div>
-          Coded by {` `}
-          <a
-            href="https://github.com/RRachelvd"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Rachèl van Dijkhorst
-          </a>
-          , open-sourced on {` `}
-          <a
-            href="https://github.com/RRachelvd/react-weather"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-          {` `}
-          and hosted on {` `}
-          <a
-            href="https://willitrainornot.netlify.app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Netlify
-          </a>
-        </div>
-      </footer>
-      <script></script>
+        <footer>
+          <div>
+            Coded by {` `}
+            <a
+              href="https://github.com/RRachelvd"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Rachèl van Dijkhorst
+            </a>
+            , open-sourced on {` `}
+            <a
+              href="https://github.com/RRachelvd/weather-app-2.0"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            {` `}
+            and hosted on {` `}
+            <a
+              href="https://react-weather-app-by-rachel.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+          </div>
+        </footer>
+        <script></script>
+      </div>
     </div>
   );
 }
