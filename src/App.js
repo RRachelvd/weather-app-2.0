45528d649a6a840298be07987e37edce0489366a
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-06-12 16:31:34 +0200

    Removed unused property

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function App() {
   return (
     <div className="weather-app-container">
-      <WeatherApp defaultCity="Leeuwarden" />
+      <WeatherApp />
 
       <div className="weather-forecast" id="forecast"></div>
       <div className="weather-forecast" id="forecast"></div>
